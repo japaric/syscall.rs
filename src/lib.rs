42c2402eb90dd2e921b4f6ffcdfa5be9952741ad
Jon Severinsson <jon@severinsson.net>
2017-05-25 00:49:06 +0200

    Handle 64 bit syscall arguments properly on 32-bit systems.
    This adds support for the keyword 'u64' to the syscall! macro, which
    causes the next argument to be treated as a 64-bit value rather than
    an usize. On 64-bit platforms, using the 'u64' keyword does nothing,
    while on 32-bit platforms it will correctly split it over two 32-bit
    registers.

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,44 @@ extern crate std;
 
 pub use platform::*;
 
+#[cfg(target_pointer_width = "64")]
+#[path="macros/64.rs"]
+pub mod macros;
+
+#[cfg(all(target_pointer_width = "32",
+          target_endian = "big",
+          any(target_arch = "arm",
+              target_arch = "mips",
+              target_arch = "powerpc",
+              target_arch = "xtensa")))]
+#[path="macros/32be-align.rs"]
+pub mod macros;
+
+#[cfg(all(target_pointer_width = "32",
+          target_endian = "big",
+          not(any(target_arch = "arm",
+                  target_arch = "mips",
+                  target_arch = "powerpc",
+                  target_arch = "xtensa"))))]
+#[path="macros/32be.rs"]
+pub mod macros;
+
+#[cfg(all(target_pointer_width = "32",
+          target_endian = "little",
+          any(target_arch = "arm",
+              target_arch = "mips",
+              target_arch = "powerpc",
+              target_arch = "xtensa")))]
+#[path="macros/32le-align.rs"]
+pub mod macros;
+
+#[cfg(all(target_pointer_width = "32",
+          target_endian = "little",
+          not(any(target_arch = "arm",
+                  target_arch = "mips",
+                  target_arch = "powerpc",
+                  target_arch = "xtensa"))))]
+#[path="macros/32le.rs"]
 pub mod macros;
 
 #[cfg(all(target_os = "linux",
